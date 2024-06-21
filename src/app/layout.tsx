d66286f04923008c812ce44ee10df8b75eda7a7b
Hudson Stolfus <hudson.stolfus@gmail.com>
2024-06-21 14:57:52 -0500

    Added navigation panel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,66 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          <nav>
+              <ul>
+                  <li>
+                      <Link href="/">
+                          SHS Sponsors
+                      </Link>
+                  </li>
+                  <hr/>
+                  <li>
+                      <form>
+                          <input type="search" placeholder="Search"/>
+                          <input type="submit"></input>
+                      </form>
+                  </li>
+                  <li>
+                      <button>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10,0C4.48,0,0,4.48,0,10s4.48,10,10,10,10-4.48,10-10S15.52,0,10,0ZM13.14,12.1l-3.14-3.14-3.14,3.14-1.41-1.41,4.56-4.56,4.56,4.56-1.41,1.41Z"/></svg>
+                          North Kansas City School District
+                      </button>
+                      <ul>
+                          <li>
+                              <Link href="/dashboard">
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 19.95">
+                                      <path
+                                          d="M9,10.36V0C3.95.5,0,4.76,0,9.95c0,5.52,4.48,10,10,10,2.4,0,4.6-.85,6.33-2.26l-7.33-7.33Z"/>
+                                      <path
+                                          d="M11,9.54l6.74,6.74c1.41-1.72,2.26-3.93,2.26-6.33,0-5.19-3.95-9.45-9-9.95v9.54Z"/>
+                                  </svg>
+                                  Dashboard
+                              </Link>
+                          </li>
+                          <li>
+                              <Link href="/partners">
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.23 20.15">
+                                      <circle cx="9.62" cy="5.08" r="5.08"/>
+                                      <path
+                                          d="M9.78,10.36C4.47,10.27.09,14.5,0,19.81l19.23.34c.09-5.31-4.14-9.69-9.45-9.78Z"/>
+                                  </svg>
+                                  Partners
+                              </Link>
+                          </li>
+                          <li>
+                              <Link href="/goals">
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27.48 27.48">
+                                      <path
+                                          d="M13.74,27.48C6.16,27.48,0,21.32,0,13.74S6.16,0,13.74,0s13.74,6.16,13.74,13.74-6.16,13.74-13.74,13.74ZM13.74,3.93c-5.41,0-9.81,4.4-9.81,9.81s4.4,9.81,9.81,9.81,9.81-4.4,9.81-9.81S19.15,3.93,13.74,3.93Z"/>
+                                      <path
+                                          d="M13.74,19.86c-3.37,0-6.12-2.75-6.12-6.12s2.75-6.12,6.12-6.12,6.12,2.75,6.12,6.12-2.75,6.12-6.12,6.12ZM13.74,11.55c-1.21,0-2.19.98-2.19,2.19s.98,2.19,2.19,2.19,2.19-.98,2.19-2.19-.98-2.19-2.19-2.19Z"/>
+                                  </svg>
+                                  Goals
+                              </Link>
+                          </li>
+                      </ul>
+                  </li>
+              </ul>
+          </nav>
+          <div id="content"></div>
+          {children}
+      </body>
     </html>
   );
 }
